eee906af9fd2ab2328a51e508d08751052754284
Valerie Kalchenko <valerie.kalchenko@gmail.com>
2023-04-25 11:51:25 -0400

    Finished Query

--- src/modules/DataQuery/index.js
+++ src/modules/DataQuery/index.js
@@ -8,15 +8,75 @@ import { Student } from '../../models';
 const DataQuery = () => {
 
     const [students, setStudents] = useState([]);
+    const [major, setmajor] = useState([]);
+    const [graduationYear, setgraduationYear] = useState([]);
+    const [courseCode, setcourseCode] = useState([]);
+
+    useEffect(() => {
+        DataStore.query(Student).then(setmajor);
+    }, []);
+
+    useEffect(() => {
+        DataStore.query(Student).then(setgraduationYear);
+    }, []);
+
+    useEffect(() => {
+        DataStore.query(Student).then(setcourseCode);
+    }, []);
+    
+
+    var arr1 = [];
+    var arr3 = [];
+    var arr4 = [];
+
+    for (let i=0; i< major.length; i++){
+        arr1.push(major[i].major)
+    }
+    var uniqueMajor = [...new Set(arr1)]
+
+    for (let i=0; i< graduationYear.length; i++){
+        arr3.push(graduationYear[i].graduationYear)
+    }
+    var uniqueYear = [...new Set(arr3)]
+
+    for (let i=0; i< courseCode.length; i++){
+        arr4.push(courseCode[i].courseCode)
+    }
+    var uniqueCode = [...new Set(arr4)]
+    
+
+    const { Option } = Select;
 
     const handleChange = (value) => {
         console.log(`${value}`);
     };
 
-    const queryStudentMajor = async ({major}) => {
+    const queryStudentMajor = async ({ major }) => {
         console.log(major);
         const queryMajor = await DataStore.query(Student, (c) => c.major.eq(major));
         setStudents(queryMajor);
+    }
+
+    const queryGradYear = async ({ graduationYear }) => {
+        console.log(graduationYear);
+        const queryYear = await DataStore.query(Student, (c) => c.graduationYear.eq(graduationYear));
+        setStudents(queryYear);
+    }
+
+    const queryCourseCode = async ({ courseCode }) => {
+        console.log(courseCode);
+        const queryCode = await DataStore.query(Student, (c) => c.courseCode.eq(courseCode));
+        setStudents(queryCode);
+    }
+
+    const queryCareerCredits = async ({ careerCredits }) => {
+        if (careerCredits >= 115){
+            const queryGreaterCredits = await DataStore.query(Student, (c) => c.careerCredits.ge(careerCredits));
+            setStudents(queryGreaterCredits);
+        } else {
+            const queryLesserCredits = await DataStore.query(Student, (c) => c.careerCredits.lt(careerCredits));
+            setStudents(queryLesserCredits);
+        }
     }
 
 
@@ -82,78 +142,45 @@ const DataQuery = () => {
                 columns={tableColumns}
                 rowKey='id'
             />
-            <Form layout='inline'style={{ marginTop: 20, }} onFinish={queryStudentMajor}>
+            <Form layout='inline' style={{ marginTop: 20, }} onFinish={queryStudentMajor}>
                 <Form.Item required name='major'>
-                    <Select defaultValue="Student Major" style={{ width: 230, padding: 5,}}
-                        onChange={handleChange}
-                        options={[
-                            {
-                                value: 'CSIT',
-                                label: 'CSIT',
-                            },
-                            {
-                                value: 'CIS',
-                                label: 'CIS',
-                            },
-                            {
-                                value: 'Cyber Security',
-                                label: 'Cyber Security',
-                            },
-                            {
-                                value: 'Website Design',
-                                label: 'Website Design',
-                            },
-                        ]}
-                    />
-                    </Form.Item>
-                    <Form.Item>
-                    <Select defaultValue="Graduation Year" style={{ width: 230, padding: 5,}}
-                        onChange={handleChange}
-                        options={[
-                            {
-                                value: '2023',
-                                label: '2023',
-                            },
-                            {
-                                value: '2024',
-                                label: '2024',
-                            },
-                            {
-                                value: '2025',
-                                label: '2025',
-                            },
-                            {
-                                value: '2026',
-                                label: '2026',
-                            },
-                        ]}
-                    />
-                    </Form.Item>
-                    <Form.Item>
-                    <Select defaultValue="Course Code" style={{ width: 230, padding: 5,}}
-                        onChange={handleChange}
-                        options={[
-                            {
-                                value: 'CSIT230',
-                                label: 'CSIT230',
-                            },
-                            {
-                                value: 'CMSC250',
-                                label: 'CMSC250',
-                            },
-                            {
-                                value: 'CSIT101',
-                                label: 'CSIT101',
-                            },
-                            {
-                                value: 'CMTC301',
-                                label: 'CMTC301',
-                            },
-                        ]}
-                    />
-                    </Form.Item>
-                    <Form.Item>
-                    <Select defaultValue="Career Credits" style={{ width: 230, padding: 5, paddingRight: 5,}}
+                    <Select defaultValue="Student Major" style={{ width: 230, padding: 5, }}>
+                    {uniqueMajor.map((uniqueMajor) => {
+                          return <Option value={uniqueMajor} key={uniqueMajor}>{uniqueMajor}</Option>
+                        })}
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Button type='primary' htmlType='submit' style={{ marginTop: 5, }}>Submit</Button>
+                </Form.Item>
+            </Form>
+            <Form layout='inline' style={{ marginTop: 20, }} onFinish={queryGradYear}>
+                <Form.Item required name='graduationYear'>
+                    <Select defaultValue="Graduation Year" style={{ width: 230, padding: 5, }}>
+                    {uniqueYear.map((uniqueYear) => {
+                          return <Option value={uniqueYear} key={uniqueYear}>{uniqueYear}</Option>
+                        })}
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Button type='primary' htmlType='submit' style={{ marginTop: 5, }}>Submit</Button>
+                </Form.Item>
+            </Form>
+            <Form layout='inline' style={{ marginTop: 20, }} onFinish={queryCourseCode}>
+                <Form.Item required name='courseCode'>
+                    <Select defaultValue="Course Code" style={{ width: 230, padding: 5, }}>
+                    {uniqueCode.map((uniqueCode) => {
+                          return <Option value={uniqueCode} key={uniqueCode}>{uniqueCode}</Option>
+                        })}
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Button type='primary' htmlType='submit' style={{ marginTop: 5, }}>Submit</Button>
+                </Form.Item>
+            </Form>
+            <Form layout='inline' style={{ marginTop: 20, }} onFinish={queryCareerCredits}>
+                <Form.Item required name='careerCredits'>
+                    <Select defaultValue="Career Credits" style={{ width: 230, padding: 5, paddingRight: 5, }}
                         onChange={handleChange}
                         options={[
                             {
@@ -167,15 +194,12 @@ const DataQuery = () => {
                         ]}
                     />
                 </Form.Item>
-                <Form.Item>
-                    <Button type='primary' htmlType='submit' style={{ marginTop: 10,}}>Major Data</Button>
+                <Form.Item >
+                    <Button type='primary' htmlType='submit' style={{ marginTop: 5, }}>Submit</Button>
                 </Form.Item>
             </Form>
         </Card>
-
-
     );
-
 };
 
 const styles = {
@@ -183,5 +207,6 @@ const styles = {
         margin: 20,
     },
 }
+
 
 export default DataQuery;
